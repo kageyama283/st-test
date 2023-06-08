c6298489dfe31498f47348fbb159ff808f1f2240
kage-96 <61382022+kage-96@users.noreply.github.com>
2023-06-08 16:23:18 +0900

    scraipingの修正

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ url = 'https://jsonplaceholder.typicode.com/posts/1'
 page = st.sidebar.selectbox('Choose your app',['Scraiping','Adtasukaru'])
 if page == 'Scraiping':
     st.title('Scraiping Page')
+    scraiping = st.button('スクレイピング開始')
+
+    if scraiping:
+        st.write('スクレイピングを開始します。')
+
 elif page == 'Adtasukaru':
     st.title('Adatasukaru Page')
 
